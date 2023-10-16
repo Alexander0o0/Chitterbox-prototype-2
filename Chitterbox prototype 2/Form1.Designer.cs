b3ddf1eb9b7d145604531c0d2cc8fe076ff69326
ak05259065 <ak05259065@LC21207XX>
2023-10-16 11:25:59 +0100

    Add project files.

---
+++ Chitterbox prototype 2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Chitterbox_prototype_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rjButton1 = new RJButton();
+            SuspendLayout();
+            // 
+            // rjButton1
+            // 
+            rjButton1.BackColor = Color.DarkMagenta;
+            rjButton1.FlatAppearance.BorderSize = 0;
+            rjButton1.FlatStyle = FlatStyle.Flat;
+            rjButton1.ForeColor = Color.White;
+            rjButton1.Location = new Point(405, 241);
+            rjButton1.Name = "rjButton1";
+            rjButton1.Size = new Size(150, 40);
+            rjButton1.TabIndex = 0;
+            rjButton1.Text = "rjButton1";
+            rjButton1.UseVisualStyleBackColor = false;
+            rjButton1.Click += rjButton1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(rjButton1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RJButton rjButton1;
+    }
+}
---
+++ Chitterbox prototype 2/Form1.cs
@@ -0,0 +1,15 @@
+namespace Chitterbox_prototype_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rjButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
